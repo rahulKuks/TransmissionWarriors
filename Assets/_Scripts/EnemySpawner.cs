0d9969266847e2d115ea57d0788558e979d9bc14
Javan <javienen@gmail.com>
2018-01-27 14:00:17 -0800

    +Spawn enemy system pass 1, we can spawn enemies on each world with a different layer mask and within the bounds set by one or several planes.

--- Assets/_Scripts/EnemySpawner.cs
+++ Assets/_Scripts/EnemySpawner.cs
@@ -7,13 +7,41 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField]
     private GameObject enemyPrefab;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private BoxCollider[] spawnPlanes;
+
+    [SerializeField]
+    private PlayerWorld playerWorld;
+
+    [SerializeField]
+    private float safeZonePercent = 0.05f;
+
+	void Start ()
+    {
+        SpawnEnemy(playerWorld.CurrentPlayerLayer);
+        SpawnEnemy(playerWorld.CurrentPlayerLayer);
+        SpawnEnemy(playerWorld.CurrentPlayerLayer);
+        SpawnEnemy(playerWorld.CurrentPlayerLayer);
+        SpawnEnemy(playerWorld.CurrentPlayerLayer);
+    }
+
+    private void SpawnEnemy(int layerMask)
+    {
+        BoxCollider randomSpawnPlane = spawnPlanes[Random.Range(0, spawnPlanes.Length - 1)];
+        
+        GameObject newEnemy = null;
+        newEnemy = Instantiate(enemyPrefab, Vector3.zero, Quaternion.identity, transform);
+
+        BoxCollider newEnemyBox = newEnemy.GetComponent<BoxCollider>();
+        if(newEnemyBox != null)
+        {
+            float xPlaneExtent = (1-safeZonePercent) * randomSpawnPlane.bounds.extents.x;
+            float zPlaneExtent = (1-safeZonePercent) * randomSpawnPlane.bounds.extents.z;
+            float RandomXinPlane = Random.Range(-xPlaneExtent, xPlaneExtent);
+            float RandomZinPlane = Random.Range(-zPlaneExtent, zPlaneExtent);
+            Vector3 spawnPosition = new Vector3(RandomXinPlane, newEnemyBox.size.y/2, RandomZinPlane) + randomSpawnPlane.transform.localPosition;
+            newEnemy.transform.localPosition = spawnPosition;
+            newEnemy.layer = layerMask;
+        }
+    }
 }
