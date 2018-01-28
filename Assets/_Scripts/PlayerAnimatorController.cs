06d23f9d0b762efa7f7716d1e822deb3cd71f996
derekbrouwer <MrMugglez>
2018-01-28 14:13:08 -0800

    added post processing + Lightsaber emission + Player Prefab animation updates.

--- Assets/_Scripts/PlayerAnimatorController.cs
+++ Assets/_Scripts/PlayerAnimatorController.cs
@@ -6,6 +6,7 @@ public class PlayerAnimatorController : MonoBehaviour
 {
 	PlayerControl player;
 	Animator anim;
+	public float rotationalYOffset;
 
 	// Use this for initialization
 	void Start () 
@@ -20,6 +21,13 @@ public class PlayerAnimatorController : MonoBehaviour
 		var locVel = transform.InverseTransformDirection (player.rb.velocity);
 		anim.SetFloat ("ForwardMovement", locVel.z);
 		anim.SetBool ("isAiming", player.isAiming());
+		if (player.isAiming ()) {
+			transform.localEulerAngles = (new Vector3 (0, rotationalYOffset, 0));
+		} 
+		else 
+		{
+			transform.localRotation = Quaternion.identity;
+		}
 		if ( (Input.GetKey(KeyCode.F) || Input.GetButtonDown("Fire1") ) && player.currentFireCD <= 0)
 		{
 			anim.SetTrigger ("Shoot");	
