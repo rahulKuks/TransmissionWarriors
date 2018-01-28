804cc4eb71ee4baf47ecb037ff789ed65af8460d
derekbrouwer <MrMugglez>
2018-01-28 15:59:31 -0800

    Enemy will always walk.

---
+++ Assets/_Scripts/EnemyAnimatorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimatorController : MonoBehaviour {
+
+	EnemyBase enemy;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		enemy = GetComponentInParent<EnemyBase> ();
+		anim = GetComponent<Animator> ();
+		anim.SetFloat ("Move", 1);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
