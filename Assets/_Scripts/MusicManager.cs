79eac18f3fe7d8ca68d94735b96954cc3e5119f0
Rahul Kukreja <Rahul_Kukreja@thecdm.ca>
2018-01-27 21:38:05 -0800

    Menu screens and flow, bgm implemented

---
+++ Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour 
+{
+    static MusicManager instance;
+
+    [SerializeField] AudioClip[] AudioClipArray;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () 
+    {
+        DontDestroyOnLoad(this);
+
+        audioSource = GetComponent<AudioSource>();
+
+        if(audioSource == null)
+        {
+            Debug.LogError("MusicManager failed to access AudioSource");
+        }
+	}
+
+    void OnEnable()
+    {
+        SceneManager.activeSceneChanged += PlayMusic;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= PlayMusic;
+    }
+
+    private void PlayMusic(Scene lastScene, Scene activeSceneChanged)
+    {
+        int arrayIndex = activeSceneChanged.buildIndex;
+
+        if(audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        if(AudioClipArray[arrayIndex] != null)
+        {
+            if(audioSource.clip != null && AudioClipArray[arrayIndex] == audioSource.clip)
+            {
+                return;
+            }
+
+            audioSource.clip = AudioClipArray[arrayIndex];
+
+            //TODO: Fade between audio clips
+
+            audioSource.Play();
+        }
+    }
+
+
+
+}
---
+++ Assets/_Scripts/ScreenManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour 
+{
+    static ScreenManager instance = null;
+
+    public void LoadLevel(string sceneName)
+    {
+        Debug.Log("Loading scene: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quitting!");
+        Application.Quit();
+    }
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
